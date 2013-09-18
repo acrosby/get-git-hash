44f6653b1710d2b8129d9b83224e14e3824de131
Alexander Crosby <acrosby@asascience.com>
2013-09-18 16:56:34 -0400

    .

--- git.py
+++ git.py
@@ -1,3 +1,7 @@
+#
+# Copyright 2013 A.Crosby
+# See LICENSE for license information
+#
 import subprocess, os
 
 def subs(cmd):
@@ -10,7 +14,6 @@ def subs(cmd):
     else:
         return out[0]
         
-
 def check(repo):
     cmd = "cd %s && git status | grep 'modified:'" % (repo,)
     modified = subs(cmd)
@@ -19,17 +22,22 @@ def check(repo):
     if len(modified) > 0 or len(new) > 0:
         raise ValueError("Please commit the changes to the repository '%s'" % (repo,))
 
+@check
 def current_hash(repo):
     cmd = "cd %s && git log | head -n 1" % (repo,)
     out = subs(cmd)
     out = out.strip("commit").strip(" ").strip("\n")
     return out
 
+@check
 def current_branch(repo):
     cmd = "cd %s && git status | grep 'On branch'" % (repo,)
     out = subs(cmd)
     out = out.strip("# On branch ").strip(" ").strip("\n")
     return out
 
+@check
 def unique(repo):
-    pass
+    branch = current_branch(repo)
+    hash = current_hash(repo)
+    return branch + "-" + hash
